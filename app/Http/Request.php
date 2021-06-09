9ac96a34d0afdde3c1485e14e2434978993c0b1a
Rafael Montes de oca <rafaelmdoh@gmail.com>
2021-06-09 16:46:34 -0500

    creacion de metodos setcontroller y setmethod

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -9,16 +9,36 @@ class Request
     // para saber que controlador requiere el usuairo
     protected $controller;
     // con que metodo vamos a responder
-    protected $method:
+    protected $method;
 
     // nuestro metodo magico constructor
-    public function __construct() {
+    public function __construct() 
+    {
         // basic.test/servicios/index
 
         // comvertimos un string a un array 
-        $this->segments = explode('/', $_SERVER['REQUEST_URI'])
+        $this->segments = explode('/', $_SERVER['REQUEST_URI']);
 
-        var_dump($_SERVER['REQUEST_URI']);
+        // comentar el llamado al metodo send() en el index
+        // var_dump($_SERVER['REQUEST_URI']);
+        // var_dump(explode('/',$_SERVER['REQUEST_URI']));
+
+        $this->setController();
+        $this->setMethod();
+    }
+    
+    public function setController()
+    {
+        $this->controller = empty($this->segments[1])
+            ? 'home'
+            : $this->segments[1];
+    }
+
+    public function setMethod()
+    {
+        $this->method = empty($this->segments[2])
+            ? 'index'
+            : $this->segments[2];
     }
 
 }
